deed04e5d0ba277d0692b0ac24c35540d323d8e2
Aliramazanian <ali.ramazanian1971@gmail.com>
2022-10-20 00:40:03 +0200

    aaaa

--- test and play code/test.js
+++ test and play code/test.js
@@ -1,5 +1,6 @@
 //## Write the code to execute a SUM between the number 12 and 20
 
-let sum = 20;
-let sum2 = 12;
-console.log(sum + sum2)
+let sum = 12;
+let sum2 = 20;
+let x = sum + sum2
+console.log(x)
